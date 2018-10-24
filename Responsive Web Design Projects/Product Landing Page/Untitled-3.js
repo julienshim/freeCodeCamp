9e654126316e072d1518f80551984a71ad288037
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-10-24 02:11:39 -0700

    splash spec design + .gitignore

--- Responsive Web Design Projects/Product Landing Page/Untitled-3.js
+++ Responsive Web Design Projects/Product Landing Page/Untitled-3.js
@@ -1,1 +1,14 @@
-// alert("Connected!");
+let ahref = document.querySelectorAll('a[href^="#"]');
+
+function smoothScrolling() {
+    ahref.forEach(anchor => {
+        anchor.addEventListener('click', function (event) {
+            event.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+}
+
+smoothScrolling();
