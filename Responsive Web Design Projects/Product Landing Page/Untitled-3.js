94b30ff0d235d1819c45399d067c5ac7bc804229
Julien Shim <25723428+julienshim@users.noreply.github.com>
2018-10-25 15:38:13 -0700

    added colorSwap js

--- Responsive Web Design Projects/Product Landing Page/Untitled-3.js
+++ Responsive Web Design Projects/Product Landing Page/Untitled-3.js
@@ -12,3 +12,24 @@ function smoothScrolling() {
 }
 
 smoothScrolling();
+
+
+let colorOption = document.querySelectorAll('.color-links');
+let displayStyle = document.querySelector('#display-style');
+let displayLimited = document.querySelector('#display-limited');
+let displayCamera = document.querySelector('#display-camera');
+
+function colorSwap() {
+    colorOption.forEach(color => {
+        color.addEventListener('click', function(){
+            isLimited = color.getAttribute('data-limited');
+            style = color.getAttribute('data-style');
+            camera = color.getAttribute('data-src');
+            displayStyle.innerHTML = style;
+            isLimited === 'yes' ? displayLimited.innerHTML = 'Limited Edition' : displayLimited.innerHTML = '';
+            displayCamera.src = camera;
+         })
+    })
+}
+
+colorSwap();
