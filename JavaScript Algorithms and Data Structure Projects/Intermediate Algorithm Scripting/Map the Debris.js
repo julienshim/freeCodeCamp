0ef9cf054448341a924dd2f3f39a26513d21bb0c
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-03-06 15:08:47 -0800

    Update Map the Debris.js

--- JavaScript Algorithms and Data Structure Projects/Intermediate Algorithm Scripting/Map the Debris.js
+++ JavaScript Algorithms and Data Structure Projects/Intermediate Algorithm Scripting/Map the Debris.js
@@ -2,9 +2,8 @@ function orbitalPeriod(arr) {
   var GM = 398600.4418;
   var earthRadius = 6367.4447;
   arr.forEach(function(obj){
-    let op = Math.round(2 * Math.PI * Math.sqrt(Math.pow((earthRadius+obj.avgAlt), 3)/GM));
+    obj.orbitalPeriod = Math.round(2 * Math.PI * Math.sqrt(Math.pow((earthRadius+obj.avgAlt), 3)/GM));
     delete obj.avgAlt
-    obj.orbitalPeriod = op;
   });
   return arr;
 }
