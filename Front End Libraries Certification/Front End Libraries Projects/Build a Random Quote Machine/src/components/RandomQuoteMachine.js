9eccc955acddb14b617c5ca1289c4b8d606b1219
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-04-14 19:55:20 -0700

    spill quotes on page

--- Front End Libraries Certification/Front End Libraries Projects/Build a Random Quote Machine/src/components/RandomQuoteMachine.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Random Quote Machine/src/components/RandomQuoteMachine.js
@@ -3,6 +3,9 @@ import React from "react";
 export default class RandomQuoteMachineApp extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      quotes: []
+    }
   }
 
   componentDidMount() {
@@ -11,6 +14,9 @@ export default class RandomQuoteMachineApp extends React.Component {
     .then(
        result => {
           console.log(result.quotes)
+          this.setState({
+            quotes: [...result.quotes]
+          })
        },
        error => {
           console.log(error);
@@ -22,6 +28,9 @@ export default class RandomQuoteMachineApp extends React.Component {
     return (
       <div id="quote-box">
         Random Quote Machine
+        {this.state.quotes.map((quote, index) => (
+        <p key={index}>{index+1}. {quote.quote} - {quote.author}!</p>
+    ))}
         <div id="text" />
         <div id="author" />
         <div id="new-quote" />
