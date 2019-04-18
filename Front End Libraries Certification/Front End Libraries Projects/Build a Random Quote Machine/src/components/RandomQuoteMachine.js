a70698b2686910e955073141b1e5132b36d58e54
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-04-17 19:51:14 -0700

    12/12 pass on requirements, basic text formatting

--- Front End Libraries Certification/Front End Libraries Projects/Build a Random Quote Machine/src/components/RandomQuoteMachine.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Random Quote Machine/src/components/RandomQuoteMachine.js
@@ -1,46 +1,61 @@
-import React from "react";
+import React from 'react';
 
 export default class RandomQuoteMachineApp extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      test: "test",
-      quotes: []
-    }
+   super(props);
+   this.state = {
+    randomQuoteIndex: 0,
+    quotes: []
+   };
   }
-
+ 
   componentDidMount() {
-    fetch("https://api.myjson.com/bins/1b0y9w")
+   fetch("https://api.myjson.com/bins/1b0y9w")
     .then(res => res.json())
     .then(
-       result => {
-          console.log(result.quotes)
-          this.setState({
-            quotes: [...result.quotes]
-          })
-       },
-       error => {
-          console.log(error);
-       }
+     result => {
+      // console.log(result.quotes);
+      this.setState({
+       quotes: [...result.quotes]
+      });
+     },
+     error => {
+      console.log(error);
+     }
     );
   }
-
+ 
+  handleNewQuote = () => {
+   this.setState({
+    randomQuoteIndex: Math.floor(Math.random() * this.state.quotes.length)
+   });
+  };
+ 
   render() {
-    const ranNum = Math.floor(Math.random()*this.state.quotes.length);
-    return (
-      <div>
-    Random Quote Machine
-    
-    {this.state.quotes.filter((val, index) => index === ranNum).map((quote, index) => (
-      <div id="quote-box" key={index}>
-        <div id="text">{quote.quote}</div> 
-        <div id="author">{quote.author}</div>
-        <a id="tweet-quote">Tweet Quote</a>
-      </div>
-    ))}
-    
-
-   </div>
-    );
+   return (
+    <div id="container">
+     <h1>Random Quote Machine</h1>
+     {this.state.quotes
+      .filter((val, index) => index === this.state.randomQuoteIndex)
+      .map((quote, index) => (
+       <div id="quote-box" key={index}>
+        <div id="text">"{quote.quote}"</div>
+        <div id="author">- {quote.author}</div>
+        <div id="buttons-container">
+         <button id="new-quote" onClick={this.handleNewQuote}>
+          New Quote
+         </button>
+         <a
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text="${quote.quote}"%0A- ${quote.author}`}
+          target="_blank"
+         >
+          Tweet Quote
+         </a>
+        </div>
+       </div>
+      ))}
+    </div>
+   );
   }
-}
+ } 
