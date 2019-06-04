883a24328310d498b2f3b19e337727141ae49441
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-06-03 19:43:16 -0700

    how did i forget about the babelrc file

--- Front End Libraries Certification/Front End Libraries Projects/Build a Markdown Preview/src/components/MarkdownPreview.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Markdown Preview/src/components/MarkdownPreview.js
@@ -1,29 +1,32 @@
 import React from "react";
 import ReactMarkdown from 'react-markdown';
 
-class MarkdownPreview extends React.Component {
+export default class MarkdownPreview extends React.Component {
   constructor(props) {
-   super(props);
-   this.state = {
-    value: '# h1\n## h2\n[link](http://julienshim.com)\n\n```codeblock```\n\n * list item \n\n > blockquote \n \n ![Alt](/wp.png "Title") \n **bold**'
-   };
+    super(props);
+    this.state = {
+      value:
+        '# h1\n## h2\n[link](http://julienshim.com)\n\n```codeblock```\n\n * list item \n\n > blockquote \n \n ![Alt](/wp.png "Title") \n **bold**'
+    };
   }
-  
+
   handleChange = (event) => {
    this.setState({value: event.target.value});
   }
- 
+
   render() {
-   // const input = {this.state.value};
-   return (
-    <div>
-      <textarea id="editor" value={this.state.value} onChange={this.handleChange} />
-     <div id="preview">
-      <ReactMarkdown source={this.state.value} 
-       escapeHtml={false}
-       />
-     </div>
-    </div>
-   );
+    // const input = {this.state.value};
+    return (
+      <div>
+        <textarea
+          id="editor"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <div id="preview">
+          <ReactMarkdown source={this.state.value} escapeHtml={false} />
+        </div>
+      </div>
+    );
   }
- }
+}
