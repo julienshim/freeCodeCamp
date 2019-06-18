4e07351322fba25f7a30f20ec4919062d70045a0
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-06-17 20:45:33 -0700

    split pink green boxes

--- Front End Libraries Certification/Front End Libraries Projects/Build a Markdown Preview/src/components/MarkdownPreview.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Markdown Preview/src/components/MarkdownPreview.js
@@ -3,42 +3,44 @@ import marked from "marked";
 
 marked.setOptions({
   gfm: true,
-  breaks: true
-});
-
-export default class MarkdownPreview extends React.Component {
+    breaks: true
+ })
+ 
+ export default class MarkdownPreview extends React.Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      markdown: ""
-    };
+   super(props);
+   this.state = {
+    markdown: ''
+   };
   }
-
+  
   componentDidMount() {
-    this.setState({
-      markdown:
-        '# a header\n\n## a subheader\n\n[a link](http://google.com)\n\n`inline code`\n\n```\nThis is code block\n```\n\n1. a list item\n\n> a block quote\n\n![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")\n\n**bolded text**'
-    });
+   this.setState({
+    markdown: '# a header\n\n## a subheader\n\n[a link](http://google.com)\n\n`inline code`\n\n```\nThis is code block\n```\n\n1. a list item\n\n> a block quote\n\n![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")\n\n**bolded text**'
+   })
   }
-
+  
   handeCreateMarkup() {
-    return { __html: marked(this.state.markdown) };
+   return {__html: marked(this.state.markdown)};
   }
-
+  
   handleChange = (event) => {
-    this.setState({ markdown: event.target.value });
-  };
-
-  render() {
-    return (
-      <div>
-        <div id="preview" dangerouslySetInnerHTML={this.handeCreateMarkup()} />
-        <textarea
-          id="editor"
-          value={this.state.markdown}
-          onChange={this.handleChange}
-        />
-      </div>
-    );
+   this.setState({markdown: event.target.value});
   }
-}
+ 
+  render() {
+   return (
+    <div id="main">
+     <div id="preview-container">
+      <h2>preview</h2>
+         <div id="preview" dangerouslySetInnerHTML={this.handeCreateMarkup()} >
+     </div>
+     </div>
+     <div id="editor-container">
+           <h2>editor</h2>
+      <textarea id="editor" value={this.state.markdown} onChange={this.handleChange} />
+     </div>
+    </div>
+   );
+  }
+ }
