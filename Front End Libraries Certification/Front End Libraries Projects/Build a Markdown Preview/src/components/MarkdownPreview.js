e00e863f9f5a12972099ba2bb43bdaa1f5f982dc
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-06-22 15:22:30 -0700

    toggle switch and overlay styling

--- Front End Libraries Certification/Front End Libraries Projects/Build a Markdown Preview/src/components/MarkdownPreview.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Markdown Preview/src/components/MarkdownPreview.js
@@ -10,8 +10,11 @@ marked.setOptions({
   constructor(props) {
    super(props);
    this.state = {
-    markdown: ''
+    markdown: '',
+    toggle: 'preview'
    };
+   this.handleToggle = this.handleToggle.bind(this);
+   this.handleChange = this.handleChange.bind(this);
   }
   
   componentDidMount() {
@@ -19,25 +22,30 @@ marked.setOptions({
     markdown: '# a header\n\n## a subheader\n\n[a link](http://google.com)\n\n`inline code`\n\n```\nThis is code block\n```\n\n1. a list item\n\n> a block quote\n\n![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo Title Text 1")\n\n**bolded text**'
    })
   }
+
+  handleToggle() {
+    this.setState(prevState => ({
+      toggle: prevState.toggle === 'preview' ? 'editor' : 'preview'
+    }));
+  }
   
   handeCreateMarkup() {
    return {__html: marked(this.state.markdown)};
   }
   
-  handleChange = (event) => {
+  handleChange(event) {
    this.setState({markdown: event.target.value});
   }
  
   render() {
    return (
     <div id="main">
-     <div id="preview-container">
-      <h2>preview</h2>
+     <div id="toggle" onClick={this.handleToggle}>{this.state.toggle}</div>
+     <div id="preview-container" className={this.state.toggle === 'preview' ? "visible" : "hidden"}>
          <div id="preview" dangerouslySetInnerHTML={this.handeCreateMarkup()} >
      </div>
      </div>
-     <div id="editor-container">
-           <h2>editor</h2>
+     <div id="editor-container" className={this.state.toggle ==='editor' ? "visible" : "hidden"}>
       <textarea id="editor" value={this.state.markdown} onChange={this.handleChange} />
      </div>
     </div>
