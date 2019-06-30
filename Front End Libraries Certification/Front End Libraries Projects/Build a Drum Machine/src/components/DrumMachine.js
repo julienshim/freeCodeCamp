e186cc113383ad81ff8cd7fd82af736826e74142
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-06-29 18:09:46 -0700

    1-5 pass

--- Front End Libraries Certification/Front End Libraries Projects/Build a Drum Machine/src/components/DrumMachine.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Drum Machine/src/components/DrumMachine.js
@@ -8,54 +8,57 @@ const bank = [
   {
     'key': 'Q',
     'id': 'Snare-01',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },
   {
     'key': 'W',
     'id': 'Snare-02',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },
   {
     'key': 'E',
     'id': 'Snare-03',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },
   {
     'key': 'A',
     'id': 'Snare-04',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },
   {
     'key': 'S',
     'id': 'Snare-05',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },
   {
     'key': 'D',
     'id': 'Snare-06',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },  
   {
     'key': 'Z',
     'id': 'Snare-07',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },
   {
     'key': 'X',
     'id': 'Snare-08',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   },
   {
     'key': 'C',
     'id': 'Snare-09',
-    'src': 'http://http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
+    'src': 'http://cdn.mos.musicradar.com/audio/samples/drum-demo-samples/CYCdh_K4-Snr05.mp3'
   }
 ]
 
 const DrumPad = (props) => {
-  const { value } = props;
+  const { value, src, id } = props;
   return(
-    <div className="drum-pad">{value}</div>
+    <div className="drum-pad" id={id}>
+      <p>{value}</p>
+      <audio className="clip"src={src} id={value}></audio>
+    </div>
   )
 }
 
@@ -67,15 +70,18 @@ export default class DrumMachine extends React.Component {
    };
   }
 
-  
- 
   render() {
    return (
     <div id="drum-machine">
      <div id="display">
       {bank.map(bankie => {
         return (
-          <DrumPad key={bankie.id} value={bankie.key} />
+          <DrumPad 
+            key={bankie.id} 
+            id={bankie.id}
+            value={bankie.key} 
+            src={bankie.src}
+          />
         )
       })}
    
