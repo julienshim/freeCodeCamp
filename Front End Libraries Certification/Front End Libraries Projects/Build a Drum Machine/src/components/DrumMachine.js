12446e338d8b3fb3191832fdfcc8142e80e89339
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-07-12 21:44:08 -0700

    react something hoc and moved audio bank to state

--- Front End Libraries Certification/Front End Libraries Projects/Build a Drum Machine/src/components/DrumMachine.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Drum Machine/src/components/DrumMachine.js
@@ -1,102 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const audioBank = [
-  {
-    'key': 'Q',
-    'id': 'Kick 1',
-    'src': './audio/ToyKit1 - Kick 1.mp3'
-  },
-  {
-    'key': 'W',
-    'id': 'Close Hat 1',
-    'src': './audio/ToyKit2 - Close Hat 1.mp3'
-  },
-  {
-    'key': 'E',
-    'id': 'Hi Hat 1',
-    'src': './audio/ToyKit3 - Hi Hat 1.mp3'
-  },
-  {
-    'key': 'R',
-    'id': 'Crash 1',
-    'src': './audio/ToyKit12 - Crash 1.mp3'
-  },
-  {
-    'key': 'T',
-    'id': 'Snare 1',
-    'src': './audio/ToyKit4 - Snare 1.mp3'
-  },
-  {
-    'key': 'Y',
-    'id': 'Tom 1',
-    'src': './audio/ToyKit11 - Tom 1.mp3'
-  },
-  {
-    'key': 'A',
-    'id': 'Kick 2',
-    'src': './audio/ToyKit5 - Kick 2.mp3'
-  },
-  {
-    'key': 'S',
-    'id': 'Close Hat 2',
-    'src': './audio/ToyKit6 - Close Hat 2.mp3'
-  },  
-  {
-    'key': 'D',
-    'id': 'Hi Hat 2',
-    'src': './audio/ToyKit7 - Hi Hat 2.mp3'
-  },
-  {
-    'key': 'F',
-    'id': 'Crash 2',
-    'src': './audio/ToyKit13 - Crash 2.mp3'
-
-  },
-  {
-    'key': 'G',
-    'id': 'Snare 2',
-    'src': './audio/ToyKit8 - Snare 2.mp3'
-
-  },
-  {
-    'key': 'H',
-    'id': 'Tom 2',
-    'src': './audio/ToyKit14 - Tom 2.mp3'
-  },
-  {
-    'key': 'Z',
-    'id': 'Kick 3',
-    'src': './audio/ToyKit9 - Kick 3.mp3'
-
-  },
-  {
-    'key': 'X',
-    'id': 'Close Hat 3',
-    'src': './audio/ToyKit10 - Close Hat 3.mp3'
-
-  },
-  {
-    'key': 'C',
-    'id': 'Horn 1',
-    'src': './audio/ToyKit15 - Horn 1.mp3'
-  },  
-  {
-    'key': 'V',
-    'id': 'Horn 2',
-    'src': './audio/ToyKit16 - Horn 2.mp3'
-  },
-  {
-    'key': 'B',
-    'id': 'Horn 3',
-    'src': './audio/ToyKit17 - Horn 3.mp3'
-  },
-  {
-    'key': 'N',
-    'id': 'Horn 4',
-    'src': './audio/ToyKit18 - Horn 4.mp3'
-  }
-]
+import {SortableContainer, SortableElement, arrayMove} from 'react-sortable-hoc';
 
 class DrumPad extends React.Component {
 
@@ -154,7 +58,103 @@ export default class DrumMachine extends React.Component {
   constructor(props) {
    super(props);
    this.state = {
-    display: 'Nothing to see here.'
+    display: 'Nothing to see here.',
+    audiobank: [
+      {
+        'key': 'Q',
+        'id': 'Kick 1',
+        'src': './audio/ToyKit1 - Kick 1.mp3'
+      },
+      {
+        'key': 'W',
+        'id': 'Close Hat 1',
+        'src': './audio/ToyKit2 - Close Hat 1.mp3'
+      },
+      {
+        'key': 'E',
+        'id': 'Hi Hat 1',
+        'src': './audio/ToyKit3 - Hi Hat 1.mp3'
+      },
+      {
+        'key': 'R',
+        'id': 'Crash 1',
+        'src': './audio/ToyKit12 - Crash 1.mp3'
+      },
+      {
+        'key': 'T',
+        'id': 'Snare 1',
+        'src': './audio/ToyKit4 - Snare 1.mp3'
+      },
+      {
+        'key': 'Y',
+        'id': 'Tom 1',
+        'src': './audio/ToyKit11 - Tom 1.mp3'
+      },
+      {
+        'key': 'A',
+        'id': 'Kick 2',
+        'src': './audio/ToyKit5 - Kick 2.mp3'
+      },
+      {
+        'key': 'S',
+        'id': 'Close Hat 2',
+        'src': './audio/ToyKit6 - Close Hat 2.mp3'
+      },  
+      {
+        'key': 'D',
+        'id': 'Hi Hat 2',
+        'src': './audio/ToyKit7 - Hi Hat 2.mp3'
+      },
+      {
+        'key': 'F',
+        'id': 'Crash 2',
+        'src': './audio/ToyKit13 - Crash 2.mp3'
+    
+      },
+      {
+        'key': 'G',
+        'id': 'Snare 2',
+        'src': './audio/ToyKit8 - Snare 2.mp3'
+    
+      },
+      {
+        'key': 'H',
+        'id': 'Tom 2',
+        'src': './audio/ToyKit14 - Tom 2.mp3'
+      },
+      {
+        'key': 'Z',
+        'id': 'Kick 3',
+        'src': './audio/ToyKit9 - Kick 3.mp3'
+    
+      },
+      {
+        'key': 'X',
+        'id': 'Close Hat 3',
+        'src': './audio/ToyKit10 - Close Hat 3.mp3'
+    
+      },
+      {
+        'key': 'C',
+        'id': 'Horn 1',
+        'src': './audio/ToyKit15 - Horn 1.mp3'
+      },  
+      {
+        'key': 'V',
+        'id': 'Horn 2',
+        'src': './audio/ToyKit16 - Horn 2.mp3'
+      },
+      {
+        'key': 'B',
+        'id': 'Horn 3',
+        'src': './audio/ToyKit17 - Horn 3.mp3'
+      },
+      {
+        'key': 'N',
+        'id': 'Horn 4',
+        'src': './audio/ToyKit18 - Horn 4.mp3'
+      }
+    ]
    };
   }
 
@@ -172,7 +172,7 @@ export default class DrumMachine extends React.Component {
       {this.state.display}
      </div>
      <div id="audio-bank">
-     {audioBank.map(element => {
+     {this.state.audiobank.map(element => {
         return (
           <DrumPad 
             key={element.id} 
