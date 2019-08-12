1dcc10907965ac8592e2ec2a574aeeb35d48ee56
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-08-12 15:55:13 -0700

    req 8 solved maitaining 11

--- Front End Libraries Certification/Front End Libraries Projects/Build a Pomodoro Clock/src/components/PomodoroClock.js
+++ Front End Libraries Certification/Front End Libraries Projects/Build a Pomodoro Clock/src/components/PomodoroClock.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { timingSafeEqual } from "crypto";
 
 export default class MarkdownPreview extends React.Component {
   constructor(props) {
@@ -10,6 +11,21 @@ export default class MarkdownPreview extends React.Component {
       current: 1500,
       isRunning: false
     };
+  }
+
+  componentDidUpdate(){
+    if(this.state.current === 0 && this.state.type === 'Session') {
+      this.setState({
+        current: this.state.break * 60,
+        type: "Break"
+      })
+    }
+    if (this.state === 0 && this.state.type === 'Break') {
+      this.setState({
+        current: this.state.session * 60,
+        type: 'Session'
+      })
+    }
   }
 
   handleStartStop = () =>  {
@@ -51,14 +67,14 @@ export default class MarkdownPreview extends React.Component {
     // If incremeanting set length and length of either type is 60 return
     if (isIncrementing && this.state[type] === 60) return;
     this.setState(prevState => ({
-      [type]: prevState[type] + (isIncrementing ? 1 : -1)
+      [type]: prevState[type] + (isIncrementing ? 1 : -1),
+      current: prevState[type] * 60 + (isIncrementing ? 60 : -60)
     }))
   }
 
   handleTimeFormat(){
     const time = this.state.current;
     // Above with pass 11 and 12 but below with pass 8. Think a method is needed for phases.
-    // const time = this.state.type === "Session" ? this.state.session * 60 : this.state.break * 60;
     const minutes = String(Math.floor(time / 60)).padStart(2,"0");
     const seconds = String(time % 60).padStart(2,"0");
     return `${minutes}:${seconds}`
