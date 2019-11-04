85305259896e40b2040d014bfcb445afa4395729
Julien Shim <25723428+julienshim@users.noreply.github.com>
2019-11-03 16:36:37 -0800

    breaking up fcc

--- Front End Libraries Certification/Front End Libraries Projects/Build a Drum Machine/src/app.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import DrumMachine from './components/DrumMachine';
-import 'normalize.css/normalize.css';
-import './styles/styles.scss';
-
-ReactDOM.render(<DrumMachine />, document.getElementById('app'));
